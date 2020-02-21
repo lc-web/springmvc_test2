f0b3a08c941cd7a3ad844aee33116da5968124db
lc-web <1206506037@qq.com>
2020-02-21 15:37:27 +0800

    本地修改同时提交

--- src/main/java/com/accp/t1/action/LoginAction.java
+++ src/main/java/com/accp/t1/action/LoginAction.java
@@ -19,6 +19,7 @@ public class LoginAction {
 		System.out.println("第一次修改");
 		System.out.println("第二次修改");
 		System.out.println("第三次修改");
+		System.out.println("第四次修改");
 		Map<String, String> message = new HashMap<String, String>();
 		if ("admin".equals(userName) && userPwd == 123) {
 			session.setAttribute("USER", new Users(userName, userPwd));
